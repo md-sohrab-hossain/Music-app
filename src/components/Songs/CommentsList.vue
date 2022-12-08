e6054cb2fcc4a3be5d4a9a80d2ad64a6b61f6638
sajalkhan <sajalhossen@yahoo.com>
2022-12-08 11:39:42 +0600

    refactor song components

---
+++ src/components/Songs/CommentsList.vue
@@ -0,0 +1,85 @@
+<template>
+  <!-- Comments -->
+  <ul class="container mx-auto">
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+    <li class="p-6 bg-gray-50 border border-gray-200">
+      <!-- Comment Author -->
+      <div class="mb-5">
+        <div class="font-bold">Elaine Dreyfuss</div>
+        <time>5 mins ago</time>
+      </div>
+
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium der doloremque laudantium.
+      </p>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "CommentsList",
+});
+</script>
---
+++ src/components/Songs/MusicHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- Music Header -->
+  <section class="w-full mb-8 py-14 text-center text-white relative">
+    <div
+      class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
+      style="background-image: url(/assets/img/song-header.png)"
+    ></div>
+    <div class="container mx-auto flex items-center">
+      <!-- Play/Pause Button -->
+      <button
+        type="button"
+        class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
+      >
+        <i class="fas fa-play"></i>
+      </button>
+      <div class="z-50 text-left ml-8">
+        <!-- Song Info -->
+        <div class="text-3xl font-bold">Song Title</div>
+        <div>Blues Rock</div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: "MusicHeader",
+};
+</script>
