715d6dc46d281e30eb6ecb592740a4d5cbe37d8a
sajalkhan <sajalhossen@yahoo.com>
2022-12-14 09:29:31 +0600

    implement error boundary component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
-  <nav-bar />
-  <!-- include router-view for routing -->
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <component :is="Component"></component>
-    </transition>
-  </router-view>
-  <!-- music player -->
-  <media-player />
-  <!-- modal -->
-  <auth-modal />
+  <error-boundary>
+    <nav-bar />
+    <!-- include router-view for routing -->
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
+    <!-- music player -->
+    <media-player />
+    <!-- modal -->
+    <auth-modal />
+  </error-boundary>
 </template>
 
 <script lang="ts">
 import NavBar from "@/components/NavBar.vue";
 import AuthModal from "@/components/AuthModal.vue";
 import MediaPlayer from "@/components/MediaPlayer.vue";
+import ErrorBoundary from "@/components/ErrorBoundary.vue";
 
 import { useUserStore } from "@/stores/user";
 import { mapWritableState } from "pinia";
@@ -27,6 +30,7 @@ export default {
     NavBar,
     MediaPlayer,
     AuthModal,
+    ErrorBoundary,
   },
   computed: {
     ...mapWritableState(useUserStore, ["userLoggedIn"]),
---
+++ src/components/ErrorBoundary.vue
@@ -0,0 +1,38 @@
+<!-- TODO: https://medium.com/js-dojo/error-exception-handling-in-vue-js-application-6c26eeb6b3e4 -->
+<template>
+  <div>
+    <slot
+      v-if="err"
+      name="error"
+      v-bind:err="err"
+      v-bind:vm="vm"
+      v-bind:info="info"
+      >Something went wrong</slot
+    >
+    <slot v-else></slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "error-boundary",
+  props: {
+    stopPropagation: Boolean,
+  },
+  data() {
+    return {
+      err: false,
+      vm: null,
+      info: null,
+    };
+  },
+  errorCaptured(err: any, vm: any, info: any) {
+    this.err = err;
+    this.vm = vm;
+    this.info = info;
+    return !this.stopPropagation;
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
