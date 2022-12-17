9e2dfefdc4278c51ffdac43588a16fe6ebf58b5e
sajalkhan <sajalhossen@yahoo.com>
2022-12-17 17:45:02 +0600

    change background music image and button color

--- src/components/Songs/MusicHeader.vue
+++ src/components/Songs/MusicHeader.vue
@@ -3,21 +3,25 @@
   <section class="w-full mb-8 py-14 text-center text-white relative">
     <div
       class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
-      style="background-image: url(/assets/img/song-header.png)"
+      style="background-image: url(/assets/img/header-12.png)"
     ></div>
-    <div class="container mx-auto flex items-center">
+    <div class="container -mt-8 mx-auto flex items-center">
       <!-- Play/Pause Button -->
       <button
         type="button"
         @click="$emit('playMusic')"
-        class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
+        class="z-50 h-24 w-24 text-3xl bg-purple-500 text-black rounded-full focus:outline-none"
       >
         <i class="fas fa-play"></i>
       </button>
       <div class="z-50 text-left ml-8">
         <!-- Song Info -->
-        <div class="text-3xl font-bold">{{ songInfo?.modified_name }}</div>
-        <div>{{ songInfo?.genre }}</div>
+        <div class="text-3xl font-bold text-green-400">
+          {{ songInfo?.modified_name }}
+        </div>
+        <div class="text-red-600 font-bold capitalize">
+          {{ songInfo?.genre }}
+        </div>
       </div>
     </div>
   </section>
