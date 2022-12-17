fe727df0163e6a3666382bcfd4f4730bbf2349d4
sajalkhan <sajalhossen@yahoo.com>
2022-12-17 14:19:47 +0600

    change header background image

--- src/components/Home/Hero.vue
+++ src/components/Home/Hero.vue
@@ -3,13 +3,15 @@
   <section class="mb-8 py-20 text-white text-center relative">
     <div
       class="absolute inset-0 w-full h-full bg-contain introduction-bg"
-      style="background-image: url(assets/img/header.png)"
+      style="background-image: url(assets/img/header-8.png)"
     />
 
     <div class="container mx-auto">
       <div class="text-white main-header-content">
-        <h1 class="mb-5 text-5xl font-bold">Listen to Great Music!</h1>
-        <p class="w-full mx-auto md:w-8/12">
+        <h1 class="mb-5 text-5xl font-bold text-red-400 cursor-default">
+          Listen to Great Music!
+        </h1>
+        <p class="w-full mx-auto md:w-8/12 text-lime-400 cursor-default">
           The perfect music gift from one of the most trusted names in
           continuing education. Learn how to better appreciate music in this
           guide that will unlock the knowledge you need to understand “the most
