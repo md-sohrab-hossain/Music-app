00603bf89387149350ccadd98b97680bad81c67c
sajalkhan <sajalhossen@yahoo.com>
2022-12-21 17:18:40 +0600

    refactor mediaPlayer design

--- src/components/MediaPlayer.vue
+++ src/components/MediaPlayer.vue
@@ -11,54 +11,57 @@
       >
     </div>
 
-    <!-- control volume -->
-    <div class="w-80 h-8 flex flex-row items-center">
-      <i class="fa fa-volume-down text-gray-500 mr-2"></i>
-      <input
-        class="h-1 w-48 form-range accent-blue-400 bg-gray-300 rounded-2xl cursor-pointer"
-        type="range"
-        min="0"
-        max="100"
-        @input.prevent="updateAudioVolume"
-        @change.prevent="updateAudioVolume"
-      />
-      <i class="fa fa-volume-up text-gray-500 ml-2"></i>
-    </div>
-
-    <div class="flex flex-nowrap gap-4 items-center">
-      <!-- Play/Pause Button -->
-      <button type="button" @click.prevent="toggleAudio">
-        <i
-          class="fa text-gray-500 text-xl"
-          :class="{ 'fa-play': !isPlaying, 'fa-pause': isPlaying }"
-        ></i>
-      </button>
-      <!-- Current Position -->
-      <div class="player-currenttime text-white">{{ seek }}</div>
-      <!-- Scrub Container  -->
-      <div
-        @click.prevent="updateSeek"
-        class="w-full h-2 rounded bg-gray-200 relative cursor-pointer"
-      >
-        <!-- Player Ball -->
-        <span
-          class="absolute -top-2.5 -ml-2.5 text-blue-400 text-lg"
-          :style="{
-            left: playerProgress,
-          }"
+    <div class="flex gap-4 items-center">
+      <!-- Player -->
+      <div class="flex flex-row w-5/6 flex-nowrap items-center gap-4">
+        <!-- Play/Pause Button -->
+        <button type="button" @click.prevent="toggleAudio">
+          <i
+            class="fa text-gray-500 text-xl"
+            :class="{ 'fa-play': !isPlaying, 'fa-pause': isPlaying }"
+          ></i>
+        </button>
+        <!-- Current Position -->
+        <div class="player-currenttime text-white">{{ seek }}</div>
+        <!-- Scrub Container  -->
+        <div
+          @click.prevent="updateSeek"
+          class="w-5/6 h-2 rounded bg-gray-200 relative cursor-pointer"
         >
-          <i class="fas fa-circle"></i>
-        </span>
-        <!-- Player Progress Bar-->
-        <span
-          class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
-          :style="{
-            width: playerProgress,
-          }"
-        ></span>
+          <!-- Player Ball -->
+          <span
+            class="absolute -top-2.5 -ml-2.5 text-blue-400 text-lg"
+            :style="{
+              left: playerProgress,
+            }"
+          >
+            <i class="fas fa-circle"></i>
+          </span>
+          <!-- Player Progress Bar-->
+          <span
+            class="block h-2 rounded bg-gradient-to-r from-green-500 to-green-400"
+            :style="{
+              width: playerProgress,
+            }"
+          ></span>
+        </div>
+        <!-- Duration -->
+        <div class="player-duration text-white">{{ duration }}</div>
       </div>
-      <!-- Duration -->
-      <div class="player-duration text-white">{{ duration }}</div>
+
+      <!-- control volume -->
+      <div class="w-auto h-8 flex flex-row items-center">
+        <i class="fa fa-volume-down text-gray-500 mr-2"></i>
+        <input
+          class="h-1 w-fit form-range accent-blue-400 bg-gray-300 rounded-2xl cursor-pointer"
+          type="range"
+          min="0"
+          max="100"
+          @input.prevent="updateAudioVolume"
+          @change.prevent="updateAudioVolume"
+        />
+        <i class="fa fa-volume-up text-gray-500 ml-2"></i>
+      </div>
     </div>
   </div>
 </template>
