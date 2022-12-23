b019841dc7ba2fea03d4d6d0bbe5b449dfb6b07e
sajalkhan <sajalhossen@yahoo.com>
2022-12-23 11:24:15 +0600

    implement about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,17 @@
 <template>
   <main>
-    <div class="about">
-      <h1>This is an about page</h1>
+    <div class="about mt-5 flex flex-col justify-center items-center">
+      <img
+        src="/assets/img/sohrab.jpg"
+        class="w-56 h-56 mb-2 object-fill rounded-full -rotate-3"
+        alt="authors"
+      />
+
+      <div class="flex flex-col items-center">
+        <p class="font-bold">Md. Sohrab hossain</p>
+        <p>Software Engineer (BJIT)</p>
+        <p>md.soharubhossen@gmail.com</p>
+      </div>
     </div>
   </main>
 </template>
