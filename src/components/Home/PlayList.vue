34b209eba1aae1f157aa985c7b3dfac1b4a39f1a
sajalkhan <sajalhossen@yahoo.com>
2022-12-23 22:41:47 +0600

    update comment position

--- src/components/Home/PlayList.vue
+++ src/components/Home/PlayList.vue
@@ -1,34 +1,34 @@
 <template>
   <!-- Main Content -->
   <section class="container mx-auto">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+    <div class="relative flex flex-col bg-white border border-gray-200 rounded">
       <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
         <span class="card-title">Songs</span>
         <!-- Icon -->
-        <i class="fa fa-headphones-alt float-right text-indigo-400 text-xl"></i>
+        <i class="float-right text-xl text-indigo-400 fa fa-headphones-alt"></i>
       </div>
 
       <!-- Playlist -->
       <ol id="playlist">
         <li v-for="song in songs" :key="song.docId">
           <div
-            class="flex justify-between items-center gap-4 p-3 pl-6 cursor-pointer transition duration-300 hover:bg-gray-50"
+            class="flex items-center justify-between gap-2 p-3 pl-6 transition duration-300 cursor-pointer hover:bg-gray-50"
           >
-            <div>
+            <div class="w-5/6 md:w-auto">
               <router-link
                 :to="{ name: 'song', params: { id: song.docId } }"
-                class="font-bold block text-gray-600"
+                class="block font-bold text-gray-600"
                 >{{ song.modified_name }}</router-link
               >
-              <span class="text-gray-500 text-sm">{{ song.display_name }}</span>
+              <span class="text-sm text-gray-500">{{ song.display_name }}</span>
             </div>
 
             <router-link
               :to="{ name: 'song', params: { id: song.docId } }"
-              class="text-gray-600 text-lg w-12"
+              class="text-lg text-gray-600 w-14 md:w-12"
             >
               <span class="comments">
-                <i class="fa fa-comments text-gray-600"></i>
+                <i class="text-gray-600 fa fa-comments"></i>
                 {{ song.comment_count }}
               </span>
             </router-link>
