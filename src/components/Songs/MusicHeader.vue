a69d44ce44231e8ed7706458877a1e41777ee69e
sajalkhan <sajalhossen@yahoo.com>
2022-12-23 10:22:30 +0600

    update play button

--- src/components/Songs/MusicHeader.vue
+++ src/components/Songs/MusicHeader.vue
@@ -8,13 +8,13 @@
     ></div>
     <div class="container -mt-8 mx-auto flex items-center">
       <!-- Play/Pause Button -->
-      <button
-        type="button"
+      <div
         @click="$emit('playMusic')"
-        class="z-50 h-24 w-24 text-3xl bg-purple-500 text-black rounded-full focus:outline-none"
+        class="playButton"
+        :class="{ buttonActive: isPlaying }"
       >
         <i class="fas fa-play"></i>
-      </button>
+      </div>
       <div class="z-50 text-left ml-8">
         <!-- Song Info -->
         <div class="text-3xl font-bold text-green-400">
@@ -48,3 +48,45 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.playButton {
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+  padding: 3rem;
+  z-index: 999999;
+  background: #efeeee;
+  border-radius: 50%;
+  transition: all 0.3s ease-in;
+  box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.1),
+    -10px -10px 10px rgba(255, 255, 255, 0.6);
+}
+.playButton .fas {
+  display: grid;
+  place-items: center;
+  font-size: 3rem;
+  color: gray;
+}
+.playButton:active {
+  box-shadow: inset 10px 10px 10px rgba(0, 0, 0, 0.1),
+    inset -10px -10px 10px rgba(255, 255, 255, 0.6);
+}
+.playButton:active .fas {
+  color: #721efa;
+}
+
+.playButton:hover,
+.buttonActive {
+  background: radial-gradient(circle, #0ff 0%, #e0e5ec 60%);
+
+  color: rgba(102, 102, 102, 0.5);
+  animation: colorChange 3s linear infinite;
+}
+
+@keyframes colorChange {
+  to {
+    filter: hue-rotate(360deg);
+  }
+}
+</style>
